e0430642106398455e6602a411521e149b7e0570
robb1ssell <robertb.webdev@gmail.com>
2020-03-20 13:41:34 -0500

    test full app deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import 'sass/App.scss';
 
+
 function App() {
+  const [passwords, setPasswords] = useState([]);
+  
+  const getPasswords = () => {
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => setPasswords(passwords))
+  }
+
+  useEffect(() => {
+    getPasswords(setPasswords)
+  }, [])
+
   return (
     <div className="App">
+      app is here
+      <p>passwords:</p>
+      {
+        passwords.length > 0 ?
+        passwords.map(pw => <p key={pw}>{pw}</p>)
+        :
+        ''
+      }
+      <button
+        onClick={getPasswords}
+      >
+        Get more passwords
+      </button>
     </div>
   );
 }
