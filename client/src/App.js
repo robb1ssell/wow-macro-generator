c97a2556e07840e0e9af577f8fc9e1ec9829a549
robb1ssell <robertb.webdev@gmail.com>
2020-03-22 13:48:37 -0500

    working google signin button

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
-  const [passwords, setPasswords] = useState([])
+  const [passwords, setPasswords] = useState([]);
+  const [userInfo, setUserInfo] = useState();
 
   const getPasswords = () => {
     fetch('/api/passwords')
       .then(res => res.json())
       .then(passwords => setPasswords(passwords))
+  }
+
+  const responseGoogle = (response) => {
+    console.log(response);
   }
 
   useEffect(() => {
@@ -16,6 +22,14 @@ function App() {
 
   return (
     <div className="App">
+      <GoogleLogin
+        clientId="836869392315-a1kdsppru2msbr38du59ck61sohlve2l.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+      <hr/>
       {
         passwords.length > 0 ?
           passwords.map(p => <p key={p}>{p}</p>)
